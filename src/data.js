7c12cf623986baee67eae67734461d94823b16e9
Andrea N <andrea@narbot.com>
2022-05-05 10:02:04 -0400

    Update data.js

--- src/data.js
+++ src/data.js
@@ -5,12 +5,14 @@ export default [
     image:
       "https://upload.wikimedia.org/wikipedia/en/9/90/RWS_Tarot_00_Fool.jpg"
   },
+  
   {
     id: 1,
     card: "The magician",
     image:
       "https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg"
   },
+  
   {
     id: 2,
     card: "The high priestess",
@@ -29,72 +31,80 @@ export default [
     image:
       "https://upload.wikimedia.org/wikipedia/en/c/c3/RWS_Tarot_04_Emperor.jpg"
   },
+  
   {
     id: 5,
     card: "The fool",
     image:
       "https://upload.wikimedia.org/wikipedia/en/8/8d/RWS_Tarot_05_Hierophant.jpg"
   },
+  
   {
     id: 6,
     card: "The lovers",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/3/3a/TheLovers.jpg"
   },
-    {
+  
+  {
     id: 7,
     card: "The chariot",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/9/9b/RWS_Tarot_07_Chariot.jpg"
   },
-    {
+  
+  {
     id: 8,
     card: "Strength",
     image:
       "https://en.wikipedia.org/wiki/Rider%E2%80%93Waite_tarot_deck#/media/File:RWS_Tarot_08_Strength.jpg"
-  },
+   },
+  
     {
     id: 9,
     card: "The Hermit",
     image:
       "https://en.wikipedia.org/wiki/Rider%E2%80%93Waite_tarot_deck#/media/File:RWS_Tarot_09_Hermit.jpg"
-  },
+   },
     {
     id: 10,
     card: "Wheel of Fortune",
     image:
       "https://en.wikipedia.org/wiki/Rider%E2%80%93Waite_tarot_deck#/media/File:RWS_Tarot_10_Wheel_of_Fortune.jpg"
-  }
-  ,
+    },
+  
     {
     id: 11,
     card: "Justice",
     image:
       "https://en.wikipedia.org/wiki/Rider%E2%80%93Waite_tarot_deck#/media/File:RWS_Tarot_11_Justice.jpg"
-  },
-    {
+    },
+  
+   {
     id: 12,
     card: "The Hanged Man",
     image:
       "https://en.wikipedia.org/wiki/Rider%E2%80%93Waite_tarot_deck#/media/File:RWS_Tarot_12_Hanged_Man.jpg"
-  },
+   },
+  
     {
     id: 13,
     card: "Death",
     image:
       "https://en.wikipedia.org/wiki/Rider%E2%80%93Waite_tarot_deck#/media/File:RWS_Tarot_13_Death.jpg"
-  }
-  ,
+    },
+  
     {
     id: 14,
     card: "Temperance",
     image:
       "https://en.wikipedia.org/wiki/Rider%E2%80%93Waite_tarot_deck#/media/File:RWS_Tarot_14_Temperance.jpg"
-  }  ,
+    },
+  
     {
     id: 15,
     card: "The Devil",
     image:
       "https://en.wikipedia.org/wiki/Rider%E2%80%93Waite_tarot_deck#/media/File:RWS_Tarot_15_Devil.jpg"
-  }
+    }
 ];
